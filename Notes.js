d7a7102c67a2a27dc10c24e6f70ee648c086f8db
bubbster95 <stiles.billy@gmail.com>
2020-08-19 19:18:51 -0400

    MVP of input panel and visual aid

---
+++ Notes.js
@@ -0,0 +1,47 @@
+function test(x) {
+    console.log(x);
+}
+/* 
+    landing page "Welcome pop up"
+        "Welcome to the segmented Ring Caluculator, a handy tool for any wood worker to get started on making segmented bowls!"
+        <button> Get started </button>
+    
+    O Creation panel
+        O Input Panel 
+            O Exterior Diameter = [User Input]
+            O Interior Diameter = [User Input]
+            O Number of Segments = [User Input]
+            O Overlap Depth = [User Input]
+            O Add New Ring = [submit]
+        
+        O Visual Aid
+            O <div classname='ring{n}'> one for each submmited ring and one live one indicating the input vlaues
+                O <div classname='exterior-diameter' color='blue'> Indicates the Exterior Ring Diameter </div>
+                O <div classname='interior-diameter-true' color='green'> Indicates interior Diameter if Overlap Depth is met </div>
+                O <div classname='interior-diameter' color='red'> Indicates interior Diameter if Overlap Depth is not met </div>
+            </div>
+
+        O Submit Rings 
+            O "button that prints results on Info Sheet"
+    O Info Sheet    
+        O Finished Visual Aid
+            O all completed rings nice and pretty
+        O Trapazoids
+            O To scale trapazoid for each ring 
+            O Inside angles written on two corners
+            O Length across trapazoids long and short side
+            O width across center of trapazoid 
+        O Specs
+            O Ring Organized by number
+                O Exterior
+                O Interior
+                O number of segments
+                O Overlap Depth
+                O Inside angles 
+                O length a
+                O length b
+                O width
+            !!Calculate length and width of each plank needed per ring
+
+            
+*/
---
+++ new-ring/new-ring.js
@@ -0,0 +1,85 @@
+let design = {};
+let n = 1;
+
+newRing = () => {
+    let newExt, newInt, newSeg, overlap = 0;
+    
+    newExt = document.querySelector('.exterior').value;
+    newInt = document.querySelector('.interior').value;
+    newSeg = document.querySelector('.segments').value;
+    overlap = document.querySelector('.overlap').value;
+    visualAidCanvas = document.querySelector('.canvas');
+
+
+    let angle = (180-(360/newSeg))/2;
+    let innerAngle = (360-(angle*2))/2;
+    let extEdge = Math.round(((Math.PI * newExt)/8)*100)/100;
+    let trapHeight = (newExt-newInt)/2;
+
+    if (newExt == '' ) {
+       document.querySelector('.exterior').style.borderColor = 'red';
+       alert("Missing exterior diameter!");
+       return;
+    } else {
+        document.querySelector('.exterior').style.borderColor = 'rgba(0,0,0,0)';
+        console.log("Exterior", newExt);
+    };
+    if (newInt == '') {
+        document.querySelector('.interior').style.borderColor = 'red';
+        alert("Missing interior diameter!");
+        return;
+    } else if (newInt > newExt-overlap) {
+        document.querySelector('.interior').style.borderColor = 'orange';
+        alert("Interior diamiter is larger then total ring width");
+    } else {
+        document.querySelector('.interior').style.borderColor = 'rgba(0,0,0,0)';
+        console.log("Interior", newInt);
+    };
+    if (newSeg == '') {
+        document.querySelector('.segments').style.borderColor = 'red';
+        alert("Missing number of segments!");
+        return;
+    } else {
+        document.querySelector('.segments').style.borderColor = 'rgba(0,0,0,0)';
+        console.log("Segments", newSeg);
+    };
+    if (overlap == '') {
+        document.querySelector('.overlap').style.borderColor = 'red';
+        alert("Missing overlap depth!");
+        return;
+    } else {
+        document.querySelector('.overlap').style.borderColor = 'rgba(0,0,0,0)';
+        console.log("Overlap", overlap);
+    }
+
+    design['ring'+n] = {
+        ext: parseInt(newExt),
+        int: parseInt(newInt),
+        segments: parseInt(newSeg),
+        overlap: parseInt(overlap),
+        extAngle: angle,
+        intAngle: innerAngle,
+        extEdge: extEdge,
+        trapHeight: trapHeight
+    };
+    
+    let ring = document.createElement('div');
+    ring.className = 'ring';
+    ring.id = 'ring' + n;
+
+    let ext = document.createElement('div');
+    ext.className = 'exterior-ring';
+    ext.style.width = (newExt*20) + 'px';
+
+    let int = document.createElement('div');
+    int.className = 'interior-ring';
+    int.style.width = (newInt*20) + 'px';
+    // ext.innerHTML = 'blank';
+    ring.appendChild(ext);
+    ring.appendChild(int);
+    visualAidCanvas.appendChild(ring);
+
+    n++;
+    console.log(design, visualAid);
+    
+}
